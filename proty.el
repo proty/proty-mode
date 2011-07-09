a58df86d44abf42e9b60de0b5c6d3a984d124ef5
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2011-07-09 14:36:27 +0200

    initial commit

---
+++ proty.el
@@ -0,0 +1,34 @@
+(defvar proty-keywords
+  '("while" "if" "else" "do" "try" "catch" "end")
+  "Proty keywords.")
+
+(defvar proty-constants
+  '("true" "false" "nil")
+  "Proty constants.")
+
+(defvar proty-keywords-regexp
+  (regexp-opt proty-keywords 'words))
+
+(defvar proty-constants-regexp
+  (regexp-opt proty-constants 'words))
+
+;; clear memory
+(setq proty-keywords nil)
+(setq proty-constants nil)
+
+(setq proty-font-lock-keywords
+      `(
+        (,proty-keywords-regexp . font-lock-keyword-face)
+        (,proty-constants-regexp . font-lock-constant-face)
+))
+
+(define-derived-mode proty-mode fundamental-mode
+  "Proty"
+  "Major mode for the Proty Programming Language"
+
+  (setq font-lock-defaults '((proty-font-lock-keywords)))
+
+  ;; clear memory
+  (setq proty-keywords-regexp nil)
+  (setq proty-constants-regexp nil)
+)
